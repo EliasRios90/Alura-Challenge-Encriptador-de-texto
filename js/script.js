5f67a58fdb60130cd66f36c4e431074e678af7ea
Alfredo Elías Ríos <117585390+EliasRios90@users.noreply.github.com>
2024-07-15 22:13:08 -0300

    Update script.js

--- js/script.js
+++ js/script.js
@@ -1,5 +1,4 @@
 let textoNoEncriptado = document.getElementById('texto__no__encriptado');
-let mensaje = document.getElementById('mensaje');
 let textoEncriptado = document.getElementById('texto__encriptado');
 
 function encriptador(){
@@ -21,6 +20,7 @@ function encriptador(){
         }
 
         textoEncriptado.value = texto;
+        // para que el resultado tenga el suficiente espacio para mostrar el texto encriptado o desencriptado
         textoEncriptado.style.height = textoEncriptado.scrollHeight+'px';
         textoNoEncriptado.value = '';
     }
@@ -44,35 +44,36 @@ function desencriptador(){
     }
 }
 
+// función para copiar al portapapeles
 function copiar(){
     textoEncriptado.select();
     textoEncriptado.setSelectionRange(0, 99999);// para dispositivos mobiles
     navigator.clipboard.writeText(textoEncriptado.value);
 }
 
-
-
+// para desbloquear el campo del resultado y mostrarlo
 function desbloquearResultado(){
     document.getElementsByClassName('contenedor__resultado__imagen')[0].style.display = 'none';
-    mensaje.style.display = 'none';
+    document.getElementById('mensaje').style.display = 'none';
     textoEncriptado.style.display = 'block';
     document.getElementById('btn__copiar').style.display = 'block';
 }
 
+// función para validar el texto del usuario. Hace una validación caracter a caracter
 function validarEntrada(){
     let tamanio = textoNoEncriptado.value.length;
     let ultimoCaracter = textoNoEncriptado.value.charCodeAt(tamanio-1);
+    let caracteres = [10, 32, 33, 44, 46, 63, 161, 191, 241];
     
-    if(ultimoCaracter == 10 || ultimoCaracter == 32 || ultimoCaracter == 241){// si ha presionado enter o es un espacio en blanco o la letra 'ñ'
-        return true;
-    }
-    else if((ultimoCaracter >= 92 && ultimoCaracter <= 122)){// si es una letra de la 'a' a la 'z' menos la letra 'ñ'
+    // 10='enter', 32=' ', 33='!', 44=',', 46='.', 63='?', 161='¡', 191='¿' y 241='ñ'
+    // [92...122] si es una letra de la 'a' a la 'z' sin la letra 'ñ'
+    if(caracteres.includes(ultimoCaracter) || (ultimoCaracter >= 92 && ultimoCaracter <= 122)){
         return true;
     }
     else{
         if(tamanio!=0){
             textoNoEncriptado.value = textoNoEncriptado.value.slice(0, -1); // borrando el ultimo caracter
-            alert('Debe ingresar solamente palabras en minúsculas.\n\nNo debe ingresar palabras en mayúsculas o caracteres especiales.');
+            alert('Debe ingresar solamente palabras en minúsculas.\n\nNo debe ingresar palabras en mayúsculas, con acentos o caracteres especiales.');
         }
         return false;
     }
